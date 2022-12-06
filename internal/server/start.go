f02b50d627eeda27d064ab8c895a8ac3c7ffbabf
xiongzhanzhang <happybear2023@163.com>
2022-12-06 23:28:01 +0800

    init frontend

--- internal/server/start.go
+++ internal/server/start.go
@@ -4,6 +4,15 @@ import "github.com/gin-gonic/gin"
 
 func StartServer() {
 	router := gin.Default()
+
+	router.Static("/static", "./frontend/dist")
+	router.StaticFile("/", "./frontend/dist/index.html")
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.Request.URL.Path = "/"
+		router.HandleContext(ctx)
+	})
+
 	registerRoutes(router)
+
 	router.Run()
 }
