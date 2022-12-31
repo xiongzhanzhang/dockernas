5cf487d5c09fb205bdf328d6ca1e02e5da676774
xiongzhanzhang <happybear2023@163.com>
2022-12-31 20:04:34 +0800

    add kodbox app

--- internal/service/network.go
+++ internal/service/network.go
@@ -133,7 +133,10 @@ func updateNginxConfig(instance models.Instance) {
 			server_name  ` + proxyConfig.HostName + "." + config.GetDomain() + `;
 	
 			location / {
-			   proxy_pass  http://host.docker.internal:` + proxyConfig.Port + `;
+				proxy_set_header Host      $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+				proxy_pass  http://host.docker.internal:` + proxyConfig.Port + `;
 			}
 		}
 		`
