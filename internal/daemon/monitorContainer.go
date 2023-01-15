411d6ccb8f9e3dc8b1a61638cf026074c6ddc3c3
xiongzhanzhang <happybear2023@163.com>
2023-01-15 22:58:38 +0800

    fix host stat monitor bug

--- internal/daemon/monitorContainer.go
+++ internal/daemon/monitorContainer.go
@@ -8,44 +8,61 @@ import (
 )
 
 var historyStatMap map[string]models.ContainerStat = map[string]models.ContainerStat{}
+var newStatMap map[string]models.ContainerStat = map[string]models.ContainerStat{}
+
+func computeSpeed(stat models.ContainerStat, curTime int64) *models.ContainerStat {
+	oldStat, ok := historyStatMap[stat.ContainerID]
+	historyStatMap[stat.ContainerID] = stat
+	newStatMap[stat.ContainerID] = stat
+	if !ok {
+		return nil
+	}
+
+	newStat := stat
+	timeGap := float64(stat.CreateTime-oldStat.CreateTime) / 1000
+	newStat.NetworkRx = (stat.NetworkRx - oldStat.NetworkRx) / timeGap
+	newStat.NetworkTx = (stat.NetworkTx - oldStat.NetworkTx) / timeGap
+	newStat.BlockRead = (stat.BlockRead - oldStat.BlockRead) / timeGap
+	newStat.BlockWrite = (stat.BlockWrite - oldStat.BlockWrite) / timeGap
+	newStat.CreateTime = curTime //reset time make chart on frontend looks good
+
+	if newStat.NetworkRx < 0 || newStat.NetworkTx < 0 || newStat.BlockRead < 0 || newStat.BlockWrite < 0 {
+		return nil
+	}
+
+	return &newStat
+}
 
 func monitorContainer() {
 	if !config.IsBasePathSet() {
 		return
 	}
 
-	models.AddContainerStat([]models.ContainerStat{GetHostState()})
+	newStatMap = map[string]models.ContainerStat{}
+
+	hostStat := computeSpeed(GetHostState(), time.Now().UnixMilli())
+	if hostStat != nil {
+		models.AddContainerStat([]models.ContainerStat{*hostStat})
+	}
 	models.DelStatDataByTime(time.Now().UnixMilli() - 7*24*60*60*1000)
 
 	var statsBySpeed []models.ContainerStat
-	var newStatMap map[string]models.ContainerStat = map[string]models.ContainerStat{}
 	containerStats := docker.GetContainerStatus()
 	curTime := time.Now().UnixMilli()
 
 	for _, stat := range containerStats {
-		newStatMap[stat.ContainerID] = stat
-		oldStat, ok := historyStatMap[stat.ContainerID]
-		if !ok {
-			continue
+		newStat := computeSpeed(stat, curTime)
+		if newStat != nil {
+			statsBySpeed = append(statsBySpeed, *newStat)
 		}
-
-		//compute io speed
-		newStat := stat
-		timeGap := float64(stat.CreateTime-oldStat.CreateTime) / 1000
-		newStat.NetworkRx = (stat.NetworkRx - oldStat.NetworkRx) / timeGap
-		newStat.NetworkTx = (stat.NetworkTx - oldStat.NetworkTx) / timeGap
-		newStat.BlockRead = (stat.BlockRead - oldStat.BlockRead) / timeGap
-		newStat.BlockWrite = (stat.BlockWrite - oldStat.BlockWrite) / timeGap
-		newStat.CreateTime = curTime //reset time make chart on frontend looks good
-
-		if newStat.NetworkRx < 0 || newStat.NetworkTx < 0 || newStat.BlockRead < 0 || newStat.BlockWrite < 0 {
-			continue
-		}
-
-		// log.Println(newStat)
-		statsBySpeed = append(statsBySpeed, newStat)
 	}
 
+	historyStatMap = newStatMap
+	models.AddContainerStat(statsBySpeed)
+	updateInstanceState()
+}
+
+func updateInstanceState() {
 	instances := models.GetInstance()
 	for _, instance := range instances {
 		if _, ok := newStatMap[instance.ContainerID]; !ok {
@@ -63,7 +80,4 @@ func monitorContainer() {
 			}
 		}
 	}
-
-	historyStatMap = newStatMap
-	models.AddContainerStat(statsBySpeed)
 }
