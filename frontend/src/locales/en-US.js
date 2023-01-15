59d8c76d3043e64c5ee188b31919d8a09e6fd14b
xiongzhanzhang <happybear2023@163.com>
2023-01-15 18:15:03 +0800

    surrport chinese first

--- frontend/src/locales/en-US.js
+++ frontend/src/locales/en-US.js
@@ -1,13 +1,30 @@
+const common = {
+  yes: '确定'
+}
+
+
 const setting = {
-  }
-  
-  const index = {
-    apps: 'apps',
-    store: 'store',
-    setting: 'setting'
-  }
-  
-  export default {
-    setting,
-    index
-  }
+}
+
+const index = {
+  apps: '应用实例',
+  store: '应用市场',
+  setting: '系统设置'
+}
+
+const store = {
+  appType: '应用类别',
+  instanceApp: '实例化应用',
+  appName: '应用名',
+  instanceName: '实例名',
+  instanceSummary: '注备',
+  appVersion: '版本'
+}
+
+// 按功能模块导出
+export default {
+  common,
+  setting,
+  index,
+  store
+}
